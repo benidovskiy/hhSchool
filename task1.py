084e68bfb233d2411a4c48ac9528d983fa231919
benidovskiy <ivan@benidovskiy.ru>
2015-10-18 05:01:40 +0300

    fix algorithm NOTE: не стоит писать код в пять утра

--- task1.py
+++ task1.py
@@ -1,5 +1,6 @@
 __author__ = 'EnDoR'
 from statistics import median
+import math
 import timeit
 import numpy.random as nprnd
 
@@ -23,12 +24,24 @@ def eff_median(array_1, array_2):
     size = len(array_1)
     i_1 = 0
     i_2 = 0
+    min1 = 0
+    max1 = 0
     while i_1 + i_2 < size - 1:
-        if array_1[i_1] < array_2[i_2]:
-            i_1 += 1
-        else:
+        if array_1[i_1] > array_2[i_2]:
             i_2 += 1
-    return 0.5 * (array_1[i_1] + array_2[i_2])
+        else:
+            i_1 += 1
+    if array_1[i_1] > array_2[i_2]:
+        min1 = array_2[i_2]
+        i_2 += 1
+    else:
+        min1 = array_1[i_1]
+        i_1 += 1
+    if array_1[i_1] > array_2[i_2]:
+        max1 = array_2[i_2]
+    else:
+        max1 = array_1[i_1]
+    return 0.5 * (min1 + max1)
 
 # Uncomment to use input from keyboard
 # arr_1 = [int(x) for x in input("Input first array: ").split()]
@@ -40,11 +53,20 @@ def eff_median(array_1, array_2):
 # print(simple_median(arr_1, arr_2))
 # print(eff_median(arr_1, arr_2))
 
-a = sorted(nprnd.randint(0, 1000, 10000))
-b = sorted(nprnd.randint(0, 1000, 10000))
+# a = sorted(nprnd.randint(0, 1000, 4))
+a = [102, 236, 404, 753]
+# b = sorted(nprnd.randint(0, 1000, 4))
+b = [201, 306, 320, 912]
+
+print(a)
+print(b)
+
+print(simple_median_statistics(a, b))
+print(simple_median(a, b))
+print(eff_median(a, b))
 
 
-print(timeit.timeit("simple_median_statistics(a,b)", number=1000,
-                    setup="from __main__ import simple_median_statistics, a, b"))
-print(timeit.timeit("simple_median(a, b)", number=1000, setup="from __main__ import simple_median, a, b"))
-print(timeit.timeit("eff_median(a, b)", number=1000, setup="from __main__ import eff_median, a, b"))
+# print(timeit.timeit("simple_median_statistics(a, b)", number=1000,
+#                     setup="from __main__ import simple_median_statistics, a, b"))
+# print(timeit.timeit("simple_median(a, b)", number=1000, setup="from __main__ import simple_median, a, b"))
+# print(timeit.timeit("eff_median(a, b)", number=1000, setup="from __main__ import eff_median, a, b"))
